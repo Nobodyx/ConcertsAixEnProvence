547b1495d2b69c11238ff629923f691d14ca1298
Nobodyx <thomas.palazzetti@ynov.com>
2017-04-11 14:40:11 +0200

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411114815) do
+ActiveRecord::Schema.define(version: 20170411123843) do
+
+  create_table "attachinary_files", force: :cascade do |t|
+    t.string   "attachinariable_type"
+    t.integer  "attachinariable_id"
+    t.string   "scope"
+    t.string   "public_id"
+    t.string   "version"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "format"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
+  end
 
   create_table "concerts", force: :cascade do |t|
     t.string   "artiste"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Concert.destroy_all
+
+def open_photo(name)
+  File.open(Rails.root.join("app", "imgConcert", name))
+end
+
+concert = Concert.create!(artiste: "Lindsey Stirling",
+  salle: "Blue Night", date: "21 Janvier 2016, 21:00" , photo: open_photo("concertBlue.jpg"))
+
+concert = Concert.create!(artiste: "Loyd Pink",
+  salle: "Pink Night", date: "15 Mars 2017, 19:00" , photo: open_photo("concertPink.jpg"))
+
+concert = Concert.create!(artiste: "Lancelot",
+  salle: "Yellow Night", date: "01 Novembre 2017, 20:15" , photo: open_photo("concertYellow.jpg"))
